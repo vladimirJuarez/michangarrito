ccfd5b747e5bc3dd00f064de02d4d1fe1f4c7fec
Vladimir Juarez <Vladimir.Juarez@audatex.com>
2021-11-26 13:29:19 -0600

    fix project

--- back/WeatherForecast.cs
+++ back/WeatherForecast.cs
@@ -1,12 +1,15 @@
-namespace michangarrito_back;
+using System;
 
-public class WeatherForecast
+namespace michangarrito_back
 {
-    public DateTime Date { get; set; }
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
 
-    public int TemperatureC { get; set; }
+        public int TemperatureC { get; set; }
 
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-    public string? Summary { get; set; }
+        public string Summary { get; set; }
+    }
 }
